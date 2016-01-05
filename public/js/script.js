c3dea9021da8c7ac9665b4957c5b3a79ee606691
Elton Santos <eltonaxl@hotmail.com>
2016-01-05 20:04:57 -0300

    Other commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,22 @@
+// Responsável por mudar as templates tags para não da conflito entre a blade do laravel e as tags do angular
 angular.module("listaTelefonica", [], function($interpolateProvider){
 	$interpolateProvider.startSymbol('<%');
 	$interpolateProvider.endSymbol('%>');
 });
+
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope){
-	$scope.app = "Phone List";
+	$scope.app = "Lista Telefônica";
+	// Lista com contatos teste
+	$scope.contatos = [
+		{nome: "Elton Santos", telefone: "986814473"},
+		{nome: "Ericson Santos", telefone: "987056766"},
+		{nome: "Lucivania Santos", telefone: "999871009"},
+		{nome: "Eloy Padilha", telefone: "988781212"},
+		{nome: "Vânia Melo", telefone: "987120902"},
+	];
+	// Botão para adicionar contato
+	$scope.adicionarContato = function(){
+		$scope.contatos.push();
+
+	};
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,54 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css" />
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <link rel="stylesheet" type="text/css" href="{{ asset('css/materialize.min.css') }}" media="screen,projection" />
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" media="screen,projection" />
     </head>
     <body ng-controller="listaTelefonicaCtrl">
         <div class="container">
-            <div>
-                <h2 class="center"><% app %></h2>
+            <div class="row col s12">
+                <div class="col l3">
+                    &nbsp;
+                </div>
+                <div class="col l6">  
+                    <div>
+                        <h2 class="teal lighten-2 truncate card-panel"><% app %></h2>
+                    </div>
+                    <table class="bordered highlight">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr ng-repeat="contato in contatos">
+                                <td><% contato.nome %></td>
+                                <td><% contato.telefone %></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br />
+                    <h6 class="card-panel teal lighten-2">Cadastrar contato</h6>
+
+                    <div class="input-field">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" class="validate" ng-model="nome">
+                        <label for="icon_prefix">Nome</label>
+                    </div>
+                    <div class="input-field">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="icon_telephone" type="text" class="validate" ng-model="telefone">
+                        <label for="icon_telephone" data-error="Errado" data-success="Correto">Telefone</label>
+                    </div>
+                    <div class="divider"></div>
+                    Nome: <% nome %>
+                    <br />
+                    Telefone: <% telefone %>
+                </div>
+                <div class="col l3">
+                     &nbsp;
+                </div>
             </div>
-            <table class="centered highlight">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Phone</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>name</td>
-                        <td>phone</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
     </body>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
